3bb84f18dc005066c53719f7613d3aa61ac208b4
Nas <1bestany@gmail.com>
2024-01-10 22:53:21 +0400

    объявлен исходный массив, прописан основной метод

---
+++ Control_task.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+string[] array1 = new string[6] { "Hello", "1567", "-13", "world", "Kazan", ":-)" };
+string[] array2 = new string[array1.Length];
+
+void CreateArray2(string[] array1, string[] array2)
+{
+    int j = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+}
