6aea67e7da508296cbac6307c1efe04810cbd2e6
Nas <1bestany@gmail.com>
2024-01-10 22:54:30 +0400

    добавлен метод печати массива

--- Control_task.cs
+++ Control_task.cs
@@ -15,3 +15,13 @@ void CreateArray2(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+CreateArray2(array1, array2);
+PrintArray(array2);
